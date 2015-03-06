1c53cc108965aca53dff2f679a7b2d10bf0f1ce0
Blair Wright <blairbwright@gmail.com>
2015-03-06 14:59:05 -0800

    blair is cool... NOT

---
+++ js/entities/entities.js
@@ -0,0 +1,19 @@
+game.PlayerEntity = me.Entity.extend({
+    init: function(x, y, settings) {
+        this._super(me.Entity, 'init', [x, y, {
+             image: "player",
+             width: 64,
+             height: 64,
+             spriteWidth: "64",
+             spriteHeight: "64",
+             getShape: function() {
+                 return(new me.Rect(0, 0, 64, 64)).toPolygon();
+             }
+             
+        }]);
+    },
+    
+    update: function(){
+        
+    }
+});
