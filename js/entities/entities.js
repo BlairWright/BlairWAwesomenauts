2e00d8f6a0a8bbab5bdb51095cd8a56abe9f7a26
Blair Wright <blairbwright@gmail.com>
2015-03-23 21:48:07 -0700

    new

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -85,16 +85,23 @@ game.PlayerEntity = me.Entity.extend({
     },
     
     collideHandler: function(response){
-        if(rsponse.b.type==='EnemyBaseEntity'){
+        if(response.b.type==='EnemyBaseEntity'){
             var ydif = this.pos.y - response.b.pos.y;
             var xdif = this.pos.x -response.b.pos.x;
             
             console.log("xdif " + xdif + " ydif " + ydif);
             
-            //if(){
-              // this.body.vel.x = 0;
-               //this.pos.x = this.pos.x -1;
-            //}
+             if(ydif<-40){
+                this.body.falling = false;
+                this.body.vel.y = -1;
+            }
+            else if(xdif>-35 && this.facing==='right' && (xdif<0)){
+               this.body.vel.x = 0;
+              this.pos.x = this.pos.x -1;
+            }else if(xdif<70 && this.facing==='left' && xdif>0){
+                this.body.vel.x = 0;
+                this.pos.x = this.pos.x +1;
+            }
         }
     }
 });
