8f4c5937a62303d307094d000357c38dbe6d31a4
Blair Wright <blairbwright@gmail.com>
2015-03-26 15:02:27 -0700

    new

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -11,7 +11,7 @@ game.PlayerEntity = me.Entity.extend({
              }
              
         }]);
-    
+    //this is where I add animations
         this.body.setVelocity(5, 20 );
         //keeps track of which direction your character is going
         this.facing = "right";
@@ -47,8 +47,8 @@ game.PlayerEntity = me.Entity.extend({
            this.jumping = true;
            this.body.vel.y -= this.body.accel.y * me.timer.tick;
        }
-        
-        
+        //^^^makes the character jump^^^
+        //▼▼▼makes the character attack▼▼▼
         if(me.input.isKeyPressed("attack")){
             if(!this.renderable.isCurrentAnimation("attack")){
                 console.log(!this.renderable.isCurrentAnimation("attack"));
@@ -238,7 +238,13 @@ game.EnemyCreep = me.Entity.extend({
         
         this._super(me.Entity, "update", [delta]);
         return true;
-    }
+    },
+    
+    collideHandler: function(response){
+        
+    } 
+    
+    
 });
 
 game.GameManager = Object.extend({
@@ -266,9 +272,14 @@ game.GameManager = Object.extend({
             this.attacking=true;
            // this.lastAttacking=this.now;
             this.body.vel.x = 0;
+            //keeps moving the creep to the right to maintain its position
             this.pos.x = this.pos.x + 1;
+            //checks that it has been at least 1 second since this creep hit a base
             if((this.now-this.lastHit >= 1000)){
+                //updates the lastHit timer
                 this.lastHit = this.now;
+                //makes  the player base call its loseHealth function and passes it at a
+                //damage of 1
                 response.b.loseHealth(1);
             }
         }
