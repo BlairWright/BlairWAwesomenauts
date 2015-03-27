a0a908fc29acd1a57095dc465a829313605e9008
Blair Wright <blairbwright@gmail.com>
2015-03-27 13:49:31 -0700

    new

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -158,7 +158,7 @@ game.EnemyBaseEntity = me.Entity.extend({
                 spriteWidth: "100",
                 spriteHeight: "100",
                 getShape: function(){
-                    return  (new me.Rect(0, 0, 100, 100)).toPolygon();
+                    return  (new me.Rect(0, 0, 100, 70)).toPolygon();
                 }
                 
         }]);
@@ -241,8 +241,23 @@ game.EnemyCreep = me.Entity.extend({
     },
     
     collideHandler: function(response){
-        
-    } 
+        console.log(response.b.type);
+        if(response.b.type==='PlayerBase'){
+            this.attacking=true;
+           // this.lastAttacking=this.now;
+            this.body.vel.x = 0;
+            //keeps moving the creep to the right to maintain its position
+            this.pos.x = this.pos.x + 1;
+            //checks that it has been at least 1 second since this creep hit a base
+            if((this.now-this.lastHit >= 1000)){
+                //updates the lastHit timer
+                this.lastHit = this.now;
+                //makes  the player base call its loseHealth function and passes it at a
+                //damage of 1
+                response.b.loseHealth(1);
+            }
+        }
+    }
     
     
 });
@@ -267,22 +282,6 @@ game.GameManager = Object.extend({
         return true;
     },
     
-    collideHandler: function(response){
-        if(resonse.b.type==='PlayerBase'){
-            this.attacking=true;
-           // this.lastAttacking=this.now;
-            this.body.vel.x = 0;
-            //keeps moving the creep to the right to maintain its position
-            this.pos.x = this.pos.x + 1;
-            //checks that it has been at least 1 second since this creep hit a base
-            if((this.now-this.lastHit >= 1000)){
-                //updates the lastHit timer
-                this.lastHit = this.now;
-                //makes  the player base call its loseHealth function and passes it at a
-                //damage of 1
-                response.b.loseHealth(1);
-            }
-        }
-    }
+    
     
 });
