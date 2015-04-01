2b59dad1a5c44a51f1ef568eb9289d111d2ac96f
Blair Wright <blairbwright@gmail.com>
2015-04-01 10:48:25 -0700

    new

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,6 +12,7 @@ game.PlayerEntity = me.Entity.extend({
              
         }]);
         this.type = "PlayerEntity";
+        this.health =20;
     //this is where I add animations
         this.body.setVelocity(5, 20 );
         //keeps track of which direction your character is going
@@ -78,7 +79,8 @@ game.PlayerEntity = me.Entity.extend({
     },
     
     loseHealth: function(damage){
-      this.health = this.health - damage;  
+      this.health = this.health - damage;
+      console.log(this.health);
     },
     
     collideHandler: function(response){
@@ -265,11 +267,12 @@ game.EnemyCreep = me.Entity.extend({
             var xdif = this.pos.x - response.b.pos.x;
             this.attacking=true;
            // this.lastAttacking=this.now;
-            this.body.vel.x = 0;
-            //keeps moving the creep to the right to maintain its position
+            
+            
             if(xdif>0){
-                console.log(xdif);
+                //keeps moving the creep to the right to maintain its position
                 this.pos.x = this.pos.x + 1;
+                this.body.vel.x = 0;
             }
             //checks that it has been at least 1 second since this creep hit something
             if((this.now-this.lastHit >= 1000) && xdif>0){
